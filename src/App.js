db2b41494964920e04166548c37bc44ea1acb796
Scott Young <youngscottj@gmail.com>
2019-09-23 18:27:43 -0600

    implement create item functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, } from 'react';
+import List from './components/list/List';
+import GroceryForm from './components/groceryForm/GroceryForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { items: [] };
+
+  getUniqId = () => {
+     return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+  renderItems = () => {
+    const { items, } = this.state;
+    return items.map( item => {
+      return (
+        <li key={item.id}>{item.name}</li>
+      )
+    })
+  };
+
+  addItem = (name) => {
+    const { items } = this.state;
+    const item = { name, id: this.getUniqId() , inCart: false }
+    this.setState({ items: [item, ...items] });
+  }
+
+  render() {
+  const { items } = this.state;
+
+    return (
+      <div>
+        < GroceryForm addItem={this.addItem} />
+        <List name = 'Grocery List' items={items} />
+      </div>
+    )
+  }
 }
 
 export default App;
